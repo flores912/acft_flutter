ff71b1e17e56c09034a2d5bcb405947e993be438
joseandresflores <flores.jose.andres.912@gmail.com>
2021-07-04 10:34:54 -0600

    -added new ui theme -home screen layout done

--- lib/acft_events.dart
+++ lib/acft_events.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
@@ -12,139 +13,79 @@ class _AcftEventsPageState extends State<AcftEventsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 2,
-        child: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              backgroundColor: Colors.black,
-              expandedHeight: 250,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                collapseMode: CollapseMode.parallax,
-                title: Text('ACFT'),
-                background: Image.asset(
-                  'images/acft_cover.jpeg',
-                  fit: BoxFit.cover,
+      appBar: AppBar(
+        title: Text('ACFT'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Main Events',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
-            ),
-            SliverPersistentHeader(
-              delegate: _SliverAppBarDelegate(
-                TabBar(
-                  labelColor: Colors.black87,
-                  unselectedLabelColor: Colors.grey,
-                  tabs: [
-                    Tab(text: "Events"),
-                    Tab(text: "Alternate"),
-                  ],
+              GridView(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3, childAspectRatio: .8),
+                  children: [
+                    EventCard(),
+                    EventCard(),
+                    EventCard(),
+                    EventCard(),
+                    EventCard(),
+                    EventCard()
+                  ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Alternate Events',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
-              pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Card(
-                  color: Colors.red,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        child: YoutubePlayer(
-                          controller: _controller,
-                          showVideoProgressIndicator: true,
-                          progressIndicatorColor: Colors.amber,
-                          progressColors: ProgressBarColors(
-                            playedColor: Colors.amber,
-                            handleColor: Colors.amberAccent,
-                          ),
-                        ),
-                        color: Colors.blueGrey,
-                        height: 150,
-                      ),
-                      ListTile(
-                        title: Text('Event'),
-                        subtitle: Text('Description'),
-                      ),
-                    ],
-                  ),
-                ),
-                Card(
-                  color: Colors.red,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        child: Center(child: Text('Image')),
-                        color: Colors.blueGrey,
-                        height: 150,
-                      ),
-                      ListTile(
-                        title: Text('Event'),
-                        subtitle: Text('Description'),
-                      ),
-                    ],
-                  ),
-                ),
-                Card(
-                  color: Colors.red,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        child: Center(child: Text('Image')),
-                        color: Colors.blueGrey,
-                        height: 150,
-                      ),
-                      ListTile(
-                        title: Text('Event'),
-                        subtitle: Text('Description'),
-                      ),
-                    ],
-                  ),
-                ),
-              ]),
-            ),
-          ],
+              ListView(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: [EventCard(), EventCard(), EventCard()]),
+            ],
+          ),
         ),
       ),
     );
   }
-
-  YoutubePlayerController _controller = YoutubePlayerController(
-    initialVideoId: 'Eef09p0NIrM',
-    flags: YoutubePlayerFlags(
-      autoPlay: false,
-      mute: false,
-      hideThumbnail: false,
-      hideControls: false,
-      controlsVisibleAtStart: false,
-    ),
-  );
 }
 
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
+class EventCard extends StatefulWidget {
   @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
+  _EventCardState createState() => _EventCardState();
+}
 
+class _EventCardState extends State<EventCard> {
   @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return new Container(
-      color: Colors.yellow,
-      child: _tabBar,
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100,
+                color: Colors.amber,
+              ),
+              ListTile(
+                title: Center(child: Text('Event')),
+              )
+            ],
+          ),
+        ),
+      ],
     );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
   }
 }
